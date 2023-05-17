c24fd45b28582f98e825cfcddbbcdc9d6346f836
Alice <lamalice9496@gmail.com>
2023-05-17 12:03:20 -0600

    setting up start button with ripple effect. using constraint layout

--- app/src/main/java/com/example/sevenminuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/sevenminuteworkout/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.sevenminuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sevenminuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.flstart.setOnClickListener{
+
+        }
     }
 }
